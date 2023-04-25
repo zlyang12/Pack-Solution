0a639d86d1be84d2a6730abf421f2f8b22109c00
zlyang12 <130017287+zlyang12@users.noreply.github.com>
2023-04-25 10:39:13 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Grace',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Pack solutions',
 
   // social usernames (optional)
   twitter: '',
